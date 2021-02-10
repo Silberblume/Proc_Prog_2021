d7bf91e9e6287f92627dfb3811305845d576da5c
Silberblume <72995429+Silberblume@users.noreply.github.com>
2021-02-10 13:55:25 +0200

    Update solution.c

--- solution.c
+++ solution.c
@@ -25,11 +25,11 @@ int main() {
 
     int under5, exact10, answer;
     failedT failed_table[32];
-
+/* Initial printing */ 
     analyze_table(YEARS, SUBJECTS, grades_table, failed_table, &under5, &exact10);
     print_data(failed_table, under5, exact10);
-
-    do{
+ /*Loop-menu, guard is 0*/
+    do{                                     
      printf("Do you want to update a grade? (1 for YES / 0 for NO): "), answer = GetInteger();
      switch (answer){
      case 0: break;
@@ -45,6 +45,10 @@ int main() {
     return 0;
 }
 
+/* This function updates (or initializes on the first time) the failed_table array. The array includes information on the number of exam failures - ironic...
+The under5 counter (which is returned) will help us read as many entries as there really are, since there can be 0<=n<=YEARS*SUBJECTS failures.
+The exact10 counter (also returend) will be needed later.  */ 
+
 void analyze_table(int years, int subjects, int grades_table[years][subjects], failedT failed_table[YEARS*SUBJECTS], int *under5, int *exact10){
     int i,j;
     (*under5) = 0;
@@ -52,7 +56,7 @@ void analyze_table(int years, int subjects, int grades_table[years][subjects], f
     for (i=0; i<YEARS ; i++){
         for (j=0; j<SUBJECTS; j++){
             if (grades_table[i][j] < 5){
-                failed_table[(*under5)].year = i;
+                failed_table[(*under5)].year = i;       
                 failed_table[(*under5)].subject = j;
                 (*under5)++;
             }
@@ -61,6 +65,8 @@ void analyze_table(int years, int subjects, int grades_table[years][subjects], f
     }
 }
 
+
+/* Here, the exact10 counter is used to calculate the number and percentage of perfect grades */ 
 void print_data(failedT failed_table[], int under5, int exact10){
     int i;
     float percentage;
@@ -76,6 +82,9 @@ void print_data(failedT failed_table[], int under5, int exact10){
 
 }
 
+/*Proper understanding of this function is paramount to the implementation of the program. 
+ This function is only called by update_data(), and is used to majorly simplify its function.
+ It basically boils down to properly reading a bounded value from the user, along with printing the right prompt*/ 
 int read_data(char msg[], int lower_lim, int upper_lim){
     int result;
     do {
@@ -83,8 +92,10 @@ int read_data(char msg[], int lower_lim, int upper_lim){
     } while (result < lower_lim || result > upper_lim);
 }
 
-void update_data(int years, int subjects,int grades_table[years][subjects]){
+/*As written above, update_data makes use of the read_data function to read values from the user.
+Those values are then used to update the grades_table.*/
 
+void update_data(int years, int subjects,int grades_table[years][subjects]){
     int i,j,val;
     char msg1[20] = "Year (0-3)";
     char msg2[20] = "Subject (0-7)";
